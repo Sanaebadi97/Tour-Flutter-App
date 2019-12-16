13be4057d61f819b82b2d9020f004f280d35e3b4
Sana Ebadi <sanaebadi97@gmail.com>
2019-12-16 23:18:03 +0330

    added some images and fonts

---
+++ lib/custom/Icon.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+
+class Icon{
+  static const IconData option = IconData(0xe902, fontFamily: "CustomIcons");
+  static const IconData menu = IconData(0xe900, fontFamily: "CustomIcons");
+}
---
+++ lib/data/data.dart
@@ -0,0 +1,19 @@
+List<String> images = [
+  "assets/images/allied.jpg",
+  "assets/images/black_book.jpg",
+  "assets/images/downfall.jpg",
+  "assets/images/fury.png",
+  "assets/images/hacksaw_ridge.png",
+  "assets/images/saving_private_ryan.png",
+  "assets/images/schindlers_liste.png",
+];
+
+List<String> title = [
+  "Allied",
+  "Black Book",
+  "Downfall",
+  "Fury",
+  "Hacksaw Ridge",
+  "Saving Private Ryan",
+  "Schindlers Liste",
+];
